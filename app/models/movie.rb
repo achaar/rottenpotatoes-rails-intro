e9f9b68e3a5719281390857daa85b3ea7f7955a4
EC2 Default User <ec2-user@ip-172-31-26-119.us-east-2.compute.internal>
2020-02-16 22:18:12 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    def self.get_all_ratings
+        arr = Array.new
+        self.select('rating').uniq.each {|ele| arr.push(ele.rating)}
+        arr.sort.uniq
+    end
 end
